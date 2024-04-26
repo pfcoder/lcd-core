eb483c32d37b0d0961c92c58649172082f621d30
chris <chris.zhu@ott.ca>
2024-04-26 23:20:00 +0800

    update

--- src/miner/avalon.rs
+++ src/miner/avalon.rs
@@ -543,28 +543,35 @@ fn tcp_cmd(ip: &str, port: u16, cmd: &str, is_waiting_write: bool) -> Result<Str
     stream.write_all(cmd.as_bytes())?;
     //info!("write done for cmd {}", cmd);
 
+    // if is_waiting_write {
+    //     let mut buf = vec![0; 10240];
+    //     let mut total_bytes_read = 0;
+
+    //     loop {
+    //         match stream.read(&mut buf[total_bytes_read..]) {
+    //             Ok(n) => {
+    //                 if n == 0 {
+    //                     break;
+    //                 }
+    //                 total_bytes_read += n;
+    //             }
+    //             Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+    //                 // Sleep for a while before trying to read again
+    //                 std::thread::sleep(Duration::from_millis(100));
+    //                 continue;
+    //             }
+    //             Err(e) => return Err(e.into()),
+    //         }
+    //     }
+
+    //     let res = String::from_utf8(buf[..total_bytes_read].to_vec())?;
+    //     //info!("avalon tcp_query result: {}", res);
+    //     return Ok(res);
+    // }
     if is_waiting_write {
-        let mut buf = vec![0; 10240];
-        let mut total_bytes_read = 0;
-
-        loop {
-            match stream.read(&mut buf[total_bytes_read..]) {
-                Ok(n) => {
-                    if n == 0 {
-                        break;
-                    }
-                    total_bytes_read += n;
-                }
-                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                    // Sleep for a while before trying to read again
-                    std::thread::sleep(Duration::from_millis(100));
-                    continue;
-                }
-                Err(e) => return Err(e.into()),
-            }
-        }
-
-        let res = String::from_utf8(buf[..total_bytes_read].to_vec())?;
+        let mut buf = [0; 10240];
+        let n = stream.read(&mut buf)?;
+        let res = String::from_utf8(buf[..n].to_vec())?;
         //info!("avalon tcp_query result: {}", res);
         return Ok(res);
     }
